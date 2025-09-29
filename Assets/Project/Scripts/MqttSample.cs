f8e133946c4dca0834a66fd91f246167eff45066
edom18 <edo.m18@gmail.com>
2025-09-30 08:11:27 +0900

    first commit.

---
+++ Assets/Project/Scripts/MqttSample.cs
@@ -0,0 +1,120 @@
+using UnityEngine;
+using MQTTnet;
+using MQTTnet.Client;
+using MQTTnet.Client.Options;
+using MQTTnet.Client.Connecting;
+using MQTTnet.Client.Receiving;
+using MQTTnet.Client.Disconnecting;
+using System.Threading;
+using System;
+using System.Text;
+using TMPro;
+using UnityEngine.UI;
+
+public class MqttSample : MonoBehaviour
+{
+    [SerializeField] private string _host = "localhost";
+    [SerializeField] private string _topic = "quest/volume";
+    [SerializeField] private Button _connectButton;
+    [SerializeField] private Button _publishButton;
+    [SerializeField] private TMP_Text _statusText;
+    [SerializeField] private TMP_Text _receivedText;
+
+    private IMqttClient _mqttClient;
+    private IMqttClientOptions _mqttClientOptions;
+
+    private void Start()
+    {
+        _connectButton.onClick.AddListener(async () =>
+        {
+            try
+            {
+                _statusText.text = "Connecting...";
+                _connectButton.interactable = false;
+                await _mqttClient.ConnectAsync(_mqttClientOptions);
+            }
+            catch (Exception e)
+            {
+                Debug.LogError($"Failed to connect to MQTT broker: {e.Message}");
+                
+                _statusText.text = "Failed to connect";
+                _connectButton.interactable = true;
+            }
+        });
+
+        _publishButton.interactable = false;
+        _publishButton.onClick.AddListener(Publish);
+
+        _mqttClient = new MqttFactory().CreateMqttClient();
+        _mqttClientOptions = new MqttClientOptionsBuilder()
+            .WithTcpServer(_host, 1883)
+            .Build();
+
+        _mqttClient.ApplicationMessageReceivedHandler = new MqttApplicationMessageReceivedHandlerDelegate(OnAppMessage);
+        _mqttClient.ConnectedHandler = new MqttClientConnectedHandlerDelegate(OnConnected);
+        _mqttClient.DisconnectedHandler = new MqttClientDisconnectedHandlerDelegate(OnDisconnected);
+    }
+
+    private void Update()
+    {
+    }
+
+    private async void OnConnected(MqttClientConnectedEventArgs args)
+    {
+        Debug.Log("MQTT broker connected.");
+
+        _statusText.text = "Connected";
+        _publishButton.interactable = true;
+
+        await _mqttClient.SubscribeAsync(new MqttTopicFilterBuilder().WithTopic(_topic).Build());
+
+        Debug.Log($"TOPIC [{_topic}] Subscribed.");
+    }
+
+    private void OnDisconnected(MqttClientDisconnectedEventArgs args)
+    {
+        if (_mqttClient == null)
+        {
+            Debug.Log("MQTT client is null or already disposed.");
+            return;
+        }
+        
+        Debug.Log("MQTT broker disconnected.");
+        
+        _statusText.text = "Disconnected";
+        _connectButton.interactable = true;
+    }
+
+    private void OnAppMessage(MqttApplicationMessageReceivedEventArgs args)
+    {
+        string payload = Encoding.UTF8.GetString(args.ApplicationMessage.Payload);
+        Debug.Log($"Received message: Topic = {args.ApplicationMessage.Topic}, Payload = {payload}");
+    }
+
+    private async void Publish()
+    {
+        try
+        {
+            Debug.Log("Publish message.");
+
+            MqttApplicationMessage message = new MqttApplicationMessageBuilder()
+                .WithTopic(_topic)
+                .WithPayload("{\"volume\":8}")
+                .WithAtLeastOnceQoS()
+                .WithRetainFlag()
+                .Build();
+
+            await _mqttClient.PublishAsync(message, CancellationToken.None);
+        }
+        catch (Exception e)
+        {
+            Debug.Log($"Failed to publish message: {e.Message}");
+        }
+    }
+
+    private void OnDestroy()
+    {
+        _mqttClient?.Dispose();
+        _mqttClient = null;
+    }
+}
